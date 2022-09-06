87935f8a91a6958c92af37f7a7f5cca3120307cb
Dzigr <dmbrag@gmail.com>
2022-09-06 15:52:03 +0300

    Fix the linter errors

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,8 @@
+"""Request for a name of user."""
 import prompt
 
 
 def welcome_user():
-    name = prompt.string ("May I have your name? ")
-    print(f"Hello, {name}!")
+    """Asking for a name of user."""
+    name = prompt.string('May I have your name? ')
+    print('Hello, {name}!'.format(name=name))
