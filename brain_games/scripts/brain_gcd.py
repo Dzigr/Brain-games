85440aab0f263edbd9b9d4f2e9ca145899f818ae
Dzigr <dmbrag@gmail.com>
2022-09-09 16:00:36 +0300

    Write divisor script

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+"""Greatest common divisor script."""
+# !/usr/bin/env python3
+from brain_games.games.gcd import divisor_checking
+
+
+def main():
+    """Greetings and checking the number for even or not."""
+    divisor_checking()
+
+
+if __name__ == '__main__':
+    main()
