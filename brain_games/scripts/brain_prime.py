e9bcac3edb03b5b986aaa2b97bd63518dc48a518
Dzigr <dmbrag@gmail.com>
2022-09-11 16:36:15 +0300

    Add script, ckeck with WPS flake8

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+"""Prime script."""
+# !/usr/bin/env python3
+from brain_games.games.prime import prime_checking
+
+
+def main():
+    """Greetings and checking prime number or not."""
+    prime_checking()
+
+
+if __name__ == '__main__':
+    main()
