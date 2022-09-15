5fe7a8daa86f6a064b3f6adafe428060ae9f93a3
Dzigr <dmbrag@gmail.com>
2022-09-15 20:49:39 +0300

    Update game_core logic

--- brain_games/games_core.py
+++ brain_games/games_core.py
@@ -1,6 +1,4 @@
 """Module with common functions for all games."""
-import random
-
 import prompt
 
 ROUNDS = 3
@@ -17,72 +15,38 @@ def welcome_user():
     return name
 
 
-def game_question(number1, number2='', sign=''):
-    """Print for question.
+def general_core(game_module):
+    """General core of all brain games.
 
-    :param:first and second number, operator if needed.
+    :param: game logic
     """
-    print('Question: {num1} {sign} {num2}'.format(
-        num1=number1, sign=sign, num2=number2,
-    ),
-    )
+    user_name = welcome_user()
+    print(game_module.GAME_RULE)
+    for _ in range(ROUNDS):
+        right_answer = game_module.game_core()
+        user_answer = prompt.string('Your answer: ')
+        if user_answer == str(right_answer):
+            print('Correct!')
+        else:
+            wrong_answer(user_answer, user_name, right_answer)
+            break
+    else:
+        print('Congratulations, {name}!'.format(name=user_name))
 
 
-def arithmetic_progression(max_step=15):
-    """Random progression.
-
-    One of the number under the random index replaced fo dots.
-
-    :return: progression, right answer
-    """
-    start = random_number()
-    step = random.randint(3, max_step)
-    progression = [num for num in range(
-        start,
-        start + step * random.randint(6, 10),
-        step
-    )]
-    dots = random.randint(1, len(progression) - 1)
-    right_answer = progression[dots]
-    progression[dots] = '..'
-    return progression, right_answer
-
-
-def random_number():
-    """Randomize a number.
-
-    :return: random number
-    """
-    return random.randint(1, 100)
-
-
-def opposite_answer(answer):
-    """Reverse answer.
-
-    return: 'yes' or 'no'
-    """
-    if answer == 'yes':
-        return 'no'
-    return 'yes'
-
-
-def wrong_answer(answer, name, right_answer):
+def wrong_answer(user_answer, user_name, right_answer):
     """
     Wrong answer of user logic.
 
-    :param: answer, name
+    :param: user answer, name, right answer
     """
-    text = """'
-    {answer}' is wrong answer ;(. Correct answer was '{right_answer}'.
+    text = """
+    \r'{answer}' is wrong answer ;(. Correct answer was '{right_answer}'.
     \rLet's try again, {name}!
     """
-    print(text.format(answer=answer, right_answer=right_answer, name=name))
-
-
-def user_answer():
-    """
-    Answer from user.
-
-    :return: answer
-    """
-    return prompt.string('Your answer: ')
+    print(text.format(
+        answer=user_answer,
+        right_answer=right_answer,
+        name=user_name,
+    ),
+    )
