9d06a7c42f625d2e9e82da2bc212624ca8e7e5e6
Dzigr <dmbrag@gmail.com>
2022-09-15 20:48:43 +0300

    Update gcd game

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,40 +1,22 @@
 """Module for greatest common divisor."""
-from brain_games.games_core import (ROUNDS, random_number, user_answer,
-                                    welcome_user, wrong_answer)
+from random import randint
+
+GAME_RULE = 'Find the greatest common divisor of given numbers.'
 
 
-def divisor_checking():
+def game_core():
     """Check for greater divisor between 2 nums.
 
-    :return: user answer, name and right answer
+    :return: right answer
     """
-    name = welcome_user()
-    count = 0
-    print('Find the greatest common divisor of given numbers.')
-    while count != ROUNDS:
-        given_numbers = (random_number(), random_number())
-        print('Question: {first_number} {second_number}'.format(
-            first_number=given_numbers[0],
-            second_number=given_numbers[1],
-        ),
-        )
-        right_answer = divisor_calculation(given_numbers)
-        answer = int(user_answer())
-        if answer == right_answer:
-            count += 1
-            print('Correct!')
-        else:
-            return wrong_answer(answer, name, right_answer)
-    print('Congratulations, {name}!'.format(name=name))
-
-
-def divisor_calculation(numbers):
-    """Mathematics logic.
-
-    :return: greatest divisor
-    """
-    for num in range(max(numbers), 0, -1):
-        if modulo(numbers, num):
+    given_numbers = (randint(1, 100), randint(1, 100))
+    print('Question: {first_number} {second_number}'.format(
+        first_number=given_numbers[0],
+        second_number=given_numbers[1],
+    ),
+    )
+    for num in range(max(given_numbers), 0, -1):
+        if modulo(given_numbers, num):
             return num
 
 
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,12 @@
 """Greatest common divisor script."""
 # !/usr/bin/env python3
-from brain_games.games.gcd import divisor_checking
+import brain_games.games.gcd as core
+from brain_games.games_core import general_core
 
 
 def main():
-    """Greetings and checking the number for even or not."""
-    divisor_checking()
+    """Greetings and checking greater divisor between 2 nums."""
+    general_core(core)
 
 
 if __name__ == '__main__':
