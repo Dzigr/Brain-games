75aae5d758d092b5ce7ac5c52706cf12df934eb8
Dzigr <dmbrag@gmail.com>
2022-09-15 20:48:15 +0300

    Update prime game

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,14 +1,16 @@
 """Module for prime numbers."""
-from brain_games.games_core import (ROUNDS, random_number, user_answer,
-                                    welcome_user, wrong_answer)
+from random import randint
+
+GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def prime_comparing(number):
-    """Compare number with the list of prime numbers.
+def game_core():
+    """Check for prime number.
 
-    :param: number
-    :return: yes or no
+    :return: right answer
     """
+    number = randint(1, 100)
+    print('Question: {requested_number}'.format(requested_number=number))
     prime_list = [
         2,
         3,
@@ -37,24 +39,3 @@ def prime_comparing(number):
         97,
     ]
     return 'yes' if number in prime_list else 'no'
-
-
-def prime_checking():
-    """Check for prime number.
-
-    :return: user answer, name and right answer
-    """
-    name = welcome_user()
-    count = 0
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    while count != ROUNDS:
-        number = random_number()
-        print('Question: {requested_number}'.format(requested_number=number))
-        right_answer = prime_comparing(number)
-        answer = user_answer()
-        if answer == right_answer:
-            count += 1
-            print('Correct!')
-        else:
-            return wrong_answer(answer, name, right_answer)
-    print('Congratulations, {name}!'.format(name=name))
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,12 @@
 """Prime script."""
 # !/usr/bin/env python3
-from brain_games.games.prime import prime_checking
+import brain_games.games.prime as core
+from brain_games.games_core import general_core
 
 
 def main():
     """Greetings and checking prime number or not."""
-    prime_checking()
+    general_core(core)
 
 
 if __name__ == '__main__':
