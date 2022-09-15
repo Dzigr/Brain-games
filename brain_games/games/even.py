81b707d3086bb7a82edb6206dee895e2ecb39761
Dzigr <dmbrag@gmail.com>
2022-09-15 20:47:56 +0300

    Update even game

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,26 +1,17 @@
 """Module for checking the even logic."""
-from brain_games.games_core import (ROUNDS, opposite_answer, random_number,
-                                    user_answer, welcome_user, wrong_answer)
+from random import randint
+
+GAME_RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def even_checking():
+def game_core():
     """Check entered number - even or not.
 
-    :return: user answer, name and right answer
+    :return: right answer
     """
-    name = welcome_user()
-    count = 0
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    while count != ROUNDS:
-        number = random_number()
-        print('Question: {random_number}'.format(random_number=number))
-        answer = user_answer()
-        if number % 2 == 0 and answer == 'yes':
-            count += 1
-            print('Correct!')
-        elif number % 2 == 1 and answer == 'no':
-            count += 1
-            print('Correct!')
-        else:
-            return wrong_answer(answer, name, opposite_answer(answer))
-    print('Congratulations, {name}!'.format(name=name))
+    number = randint(1, 100)
+    print('Question: {random_number}'.format(random_number=number))
+    if number % 2 == 0:
+        return 'yes'
+    elif number % 2 == 1:
+        return 'no'
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,12 @@
 """Even script."""
 # !/usr/bin/env python3
-from brain_games.games.even import even_checking
+import brain_games.games.even as core
+from brain_games.games_core import general_core
 
 
 def main():
     """Greetings and checking the number for even or not."""
-    even_checking()
+    general_core(core)
 
 
 if __name__ == '__main__':
